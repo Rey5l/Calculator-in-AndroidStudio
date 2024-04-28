5ddf7308fc729118c0da65aa32fe9b99a2da0f05
Reysl <rasulnasaraliev@gmail.com>
2024-04-28 17:41:26 +0300

    Работа с дизайном

--- app/src/main/java/com/reysl/learnAndroidStudio/MainActivity.java
+++ app/src/main/java/com/reysl/learnAndroidStudio/MainActivity.java
@@ -1,60 +1,12 @@
 package com.reysl.learnAndroidStudio;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btnFragment1, btnFragment2;
-    FrameLayout frameLayout;
-    private FirstFragment firstFragment = new FirstFragment();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnFragment1 = findViewById(R.id.btn_fragment1);
-        btnFragment2 = findViewById(R.id.btn_fragment2);
-        frameLayout = findViewById(R.id.frame);
-
-        setNewFragment(firstFragment);
-
-        btnFragment1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setNewFragment(firstFragment);
-            }
-        });
-
-        btnFragment2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SecondFragment secondFragment = new SecondFragment();
-                setNewFragment(secondFragment);
-            }
-        });
-    }
-
-    private void setNewFragment(Fragment fragment) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frame, fragment);
-        ft.addToBackStack(null); // чтобы память телефона не замусорилась
-        ft.commit();
     }
 }
