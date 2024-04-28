14c21027e9fe6b90f9cac8ace574a98e57a0ccda
Reysl <rasulnasaraliev@gmail.com>
2024-04-28 18:19:19 +0300

    Создание простого текстового редактора

--- app/src/main/java/com/reysl/learnAndroidStudio/MainActivity.java
+++ app/src/main/java/com/reysl/learnAndroidStudio/MainActivity.java
@@ -1,12 +1,68 @@
 package com.reysl.learnAndroidStudio;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText bio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.user_name_field);
+        bio = findViewById(R.id.user_bio_field);
+    }
+
+    /** @noinspection CallToPrintStackTrace*/
+    public void saveData(View view) {
+        String userName = name.getText().toString();
+        String userBio = bio.getText().toString();
+
+        try {
+            FileOutputStream fileOutput = openFileOutput("user_data.txt", MODE_PRIVATE);
+            fileOutput.write((userName + ". " + userBio).getBytes());
+            fileOutput.close();
+
+            name.setText("");
+            bio.setText("");
+            Toast.makeText(this, "Текст сохранен", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Не можем обработать файл", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void getDate(View view) {
+        try {
+            FileInputStream fileInput = openFileInput("user_data.txt");
+            InputStreamReader reader = new InputStreamReader(fileInput);
+            BufferedReader bR = new BufferedReader(reader);
+
+            String lines = "";
+            StringBuilder result = new StringBuilder();
+            while((lines = bR.readLine()) != null) {
+                result.append(lines).append("\n");
+            }
+
+            Toast.makeText(this, result.toString(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
